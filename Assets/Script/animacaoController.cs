8c9fd5e70b19d7c38c5b9230a97ef3adc7defe91
Lucas <lucasrchaves1@gmail.com>
2019-05-05 13:38:22 -0300

    Realizando duas animações

---
+++ Assets/Script/animacaoController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animacaoController: MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Animator animator;
+    void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator> (); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown (KeyCode.D)){
+            // dancando verdadeiro
+            animator.SetBool("dancando", true);
+        }
+
+        if(Input.GetKeyUp (KeyCode.D)){
+            // dancando false
+            animator.SetBool("dancando", false);
+
+        }
+    }
+}
